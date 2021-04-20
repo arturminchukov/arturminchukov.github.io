313d2e4a253238c9422b8191a591a21bc755072a
Artur Minchukov <arthurminchukov@yandex.ru>
2021-04-20 08:56:59 +0300

    Added birthday test

---
+++ birthday-test/precache-manifest.617971655f95f53bef7063e806c4f5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f0f322b86147cb639e92798fc2df05",
+    "url": "/birthday-test/index.html"
+  },
+  {
+    "revision": "992fcf2a7a79a5c5c45e",
+    "url": "/birthday-test/static/css/main.158c03a4.chunk.css"
+  },
+  {
+    "revision": "74a1efa7b383a0a9284b",
+    "url": "/birthday-test/static/js/2.eecde479.chunk.js"
+  },
+  {
+    "revision": "992fcf2a7a79a5c5c45e",
+    "url": "/birthday-test/static/js/main.b95ec69b.chunk.js"
+  },
+  {
+    "revision": "d729f6178a5164901656",
+    "url": "/birthday-test/static/js/runtime~main.244a22a1.js"
+  },
+  {
+    "revision": "8dc922e172de72714a99a6c3d7fd83ac",
+    "url": "/birthday-test/static/media/petal.8dc922e1.png"
+  }
+]);
---
+++ birthday-test/precache-manifest.ef7ce8f3a444c92c5aaa710682e89a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3217e61efc0f816eaa2cc662e878c558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "777a704be54aaa7ff948",
+    "url": "/static/css/main.723ad3f4.chunk.css"
+  },
+  {
+    "revision": "74a1efa7b383a0a9284b",
+    "url": "/static/js/2.eecde479.chunk.js"
+  },
+  {
+    "revision": "777a704be54aaa7ff948",
+    "url": "/static/js/main.28576a7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8dc922e172de72714a99a6c3d7fd83ac",
+    "url": "/static/media/petal.8dc922e1.png"
+  }
+]);
